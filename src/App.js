1b572141cd7c5b7327c8d326b4e4bf346950766c
ChaudharyAbhinav <contact.abhinavc@gmail.com>
2023-03-04 14:34:46 +0530

    done with react router

--- src/App.js
+++ src/App.js
@@ -3,16 +3,53 @@ import ReactDOM from "react-dom/client";
 import Body from "./components/Body";
 import Header from "./components/Header";
 import Footer from "./components/Footer"
+import About from "./components/About";
+import Errormessage from "./components/Errormessage";
+import Contact from "./components/Contact";
+import Login from "./components/Login";
+import RestaurantMenu from "./components/RestaurantMenu";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+
 
 const Applayout= ()=>{
-    return(<>
+    return(<React.Fragment>
     <Header/>
-    <Body/>
+    <Outlet/>
 
     <Footer/>
-    </>);
+    </React.Fragment>);
 }
 
-const root= ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<Applayout/>);
+const appRouter = createBrowserRouter([
+  {
+    path: "/", // show path for routing
+    element: <Applayout />, // show component for particular path
+    errorElement: <Errormessage />, // show error component for path is different
+    children: [
+      // show children component for routing
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/restaurant/:resId",
+        element: <RestaurantMenu />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<RouterProvider router={appRouter} />);
---
+++ src/components/About.js
@@ -0,0 +1,21 @@
+import food from "../Assets/burger.jpeg";
+
+const About = () => {
+  return (
+    <div className="about-container">
+      <div className="about-left">
+        <h1>
+          Welcome to <br /> The world of <br /> <span>Tasty & Fresh Food</span>
+        </h1>
+        <h4>
+          "Elevate your senses with each bite, <span>deliciousness awaits!</span>"
+        </h4>
+      </div>
+      <div className="about-right">
+        <img src={food} alt="Food Image" />
+      </div>
+    </div>
+  );
+};
+
+export default About;
