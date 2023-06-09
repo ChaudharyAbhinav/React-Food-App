b1ceb439c6ef9a9f507ce6a01426939d9c95811d
ChaudharyAbhinav <contact.abhinavc@gmail.com>
2023-06-09 23:55:20 +0530

    started with components

--- App.js
+++ App.js
@@ -1,61 +1,56 @@
-// ## Namaste React Course by Akshay Saini
-// # Chapter 02 - Igniting our App
-//Parcel is a beast
-/*
- *** Parcel Feature ***
- * Created A Server
- * HMR - Hot Module Replacement
- * File Watcher algorithm - C++
- * BUNDLING
- * MINIFY
- * Cleaning our Code
- * Dev and Production Build
- * Super Fast build algorithm
- * Image Optimization
- * Caching while development
- * Compression
- * Compatible with older version of browser
- * HTTPS on dev
- * port Number
- * Consistent Hashing Algorithm
- * Zero Config
- * Tree shaking - removing unwanted code 
- *
- *
- * Transitive Dependencies
- */
-
-
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-    "h1",
-    {
-        id:"title"
-    },
-    //"Hi from Abhinav"
-    [React.createElement(
-    "h1",
-    {
-        id:"title"
-    },
-    "Hi from Abhinav"
 
-),React.createElement(
-    "h1",
-    {
-        id:"title"
-    },
-    "Hi from Abhinav"
-
-)
-
-]
+const Intro = (
+    <h1> Hi i am a react element</h1>
 );
 
+const HeadComponent = ()=>{
+    return <div><h1>I am HeadComponent</h1></div>;
+};
+
+const HeadComponent2 = ()=>{
+    return (
+    <div>
+        <h1>I am HeadComponent2</h1>
+    </div>
+);
+};
+
+const HeadComponent3=()=>(
+    <div>
+        <h1>I am HeadComponent3</h1>
+    </div> 
+);
+
+// const data = api.getData();
+const HeadComponent4 = function(){
+    return(
+    <div>
+            {/* For React Element */}
+        {Intro} 
+        {/* For React Componenet */}
+        <HeadComponent2/> 
+        {/* or for react component we can write */}
+        {HeadComponent3()}
+        <h1>Abhinav asdad</h1>
+    </div>
+    );
+};
+
+
+var xyz = 10;
+const Display = ()=>{
+    return (
+        <>
+        {/* YOu can write any javascript code within {}  */}
+        {1 + 2}
+        {xyz}
+        </>
+    );
+};
 
 const root= ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(heading);
+root.render(<Display/>);
