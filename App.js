abffae8583e189a5d7610c99505468f8d2e2793f
ChaudharyAbhinav <contact.abhinavc@gmail.com>
2023-06-10 12:08:56 +0530

    rendering hard coded data

--- App.js
+++ App.js
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import FoodLogo from "./Assets/logo.jpg"
 const Title = ()=>(
-    <img className="logo" alt="Logo" src={FoodLogo}/>
+    <a href="/">
+        <img className="logo" alt="Logo" src={FoodLogo}/>
+    </a>
 );
 
 const Header = ()=>{
@@ -20,9 +22,37 @@ return (
     </div>);
 };
 
-const Body =function(){
-   return ( <h2>Body</h2>);
+const RestaurantCard =function(){
+   return (
+    <div className="card">
+    <img src={BurgerKing.img}/>
+    <h2>{BurgerKing.name}</h2>
+    <h3>{BurgerKing.cusines.join(",")}</h3>
+    <h4>{BurgerKing.rating} stars</h4>
+    </div>
+   );
 };
+
+const BurgerKing ={
+    "name":"BurgerKing",
+    "img":"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/xofljpukrkkbftsjospa",
+    "cusines":["Burger","Fries"],
+    "rating":"4.2"
+}
+
+const Body =()=>(
+    <div className="restaurant-list">
+    <RestaurantCard />
+       <RestaurantCard/>
+
+   <RestaurantCard/>
+
+   <RestaurantCard/>
+
+   <RestaurantCard/>
+    </div>
+
+)
 
 const Footer = ()=>{
     return(<h3>Footer</h3>);
@@ -32,6 +62,7 @@ const Applayout= ()=>{
     return(<>
     <Header/>
     <Body/>
+
     <Footer/>
     </>);
 }
