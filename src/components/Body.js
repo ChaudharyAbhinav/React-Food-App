0f43d936a7648f47b659622bbb78b74efa0081f5
ChaudharyAbhinav <contact.abhinavc@gmail.com>
2023-06-10 22:06:03 +0530

    implemented search using useState

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,49 @@
 import RestaurantCard from "./RestaurantCard";
 import { Restaurantlist } from "../constants";
-const Body =()=>(
+import { useState } from 'react';
+
+
+function filterData(inputtext, restaurants) {
+  const filterData = restaurants.filter((restaurant) =>
+    restaurant?.data?.name.toLowerCase().includes(inputtext.toLowerCase())
+  );
+  return filterData;
+}
+
+
+ const Body =()=>{
+  
+    const [inputtext , setInputtext] = useState("");
+    const [restautants,setRestaurants] =useState(Restaurantlist);
+    return(
+        <>
+    <div className="search-container">
+        <input 
+        type="text" 
+        value={inputtext} 
+        onChange={(e)=>setInputtext(e.target.value)
+        }>             
+        </input>
+        <button 
+        className="submit-btn" 
+        onClick={()=>{
+              const result = filterData(inputtext,restautants);  
+               setRestaurants(result) 
+            }
+        }>search</button>
+    </div>
     <div className="restaurant-list">
-        {Restaurantlist.map((restaurant)=>{
-            return <RestaurantCard key={restaurant.data.id} {...restaurant.data}/>
+
+        {restautants.map((restaurant)=>{
+            return( <RestaurantCard key={restaurant.data.id} {...restaurant.data}/>
+            );
         })}
     </div>
 
+        </>
 )
 
+}
 
 export default Body;
+
